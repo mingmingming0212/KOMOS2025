7a66f83969fcaee5531d256e4c56962635ecb935
Minchul <minchulshin.2023@scis.smu.edu.sg>
2025-02-06 18:22:25 +0800

    Controller test

---
+++ website/src/main/java/com/komos2025/website/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.komos2025.website.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "index.html";
+    }
+}
